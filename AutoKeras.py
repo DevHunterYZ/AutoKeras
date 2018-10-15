ad658fc71a78af681070ebea5a11851e1c29664c
DevHunter <32465888+DevHunterYZ@users.noreply.github.com>
2018-10-15 03:21:11 +0300

    Create AutoKeras.py

---
+++ AutoKeras.py
@@ -0,0 +1,22 @@
+from keras.datasets import mnist
+from autokeras.classifier import ImageClassifier
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train = x_train.reshape(x_train.shape + (1,))
+x_test = x_test.reshape(x_test.shape + (1,))
+
+clf = ImageClassifier(verbose=True, searcher_args={'trainer_args':{'max_iter_num':5}})
+
+clf.fit(x_train, y_train, time_limit=5 * 60 * 60)
+
+clf.final_fit(x_train, y_train, x_test, y_test, retrain=False, trainer_args={'max_iter_num':10})
+
+y = clf.evaluate(x_test, y_test)
+print(y * 100)
+
+best_model = clf.load_searcher().load_best_model()
+
+print(best_model.n_layers)
+
+from torchvision import models
+print(best_model.produce_model())
