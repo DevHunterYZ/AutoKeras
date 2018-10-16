4b4193010736e43e01449c3e2084b5b1acd84a27
DevHunter <32465888+DevHunterYZ@users.noreply.github.com>
2018-10-16 16:48:55 +0300

    Update AutoKeras.py

--- AutoKeras.py
+++ AutoKeras.py
@@ -1,6 +1,7 @@
+# Load MNIST data and AutoKeras library
 from keras.datasets import mnist
 from autokeras.classifier import ImageClassifier
-
+# Set up test and training set
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train = x_train.reshape(x_train.shape + (1,))
 x_test = x_test.reshape(x_test.shape + (1,))
